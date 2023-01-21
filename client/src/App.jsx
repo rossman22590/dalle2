d6972bffba33d761074663101a55c5646efdf9ae
Lucas Trecente <lucastrecente@hotmail.com>
2023-01-21 16:39:36 -0300

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { logo } from './assets'
+
+export default function App() {
+  return (
+    <h1 className='text-3xl font-bold underline'>DALL-E 2.0</h1>
+  )
+}
