32aef91c94efc7ed9d3844fe9920805535665bf9
Lucas Trecente <lucastrecente@hotmail.com>
2023-01-23 11:47:45 -0300

    feat: show allPosts on home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,6 +17,32 @@ export function Home() {
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState('');
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        if (response.ok) {
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error)
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -46,7 +72,7 @@ export function Home() {
                 />
               ) : (
                 <RenderCards
-                  data={[]}
+                  data={allPosts}
                   title='No posts founds'
                 />
               )}
